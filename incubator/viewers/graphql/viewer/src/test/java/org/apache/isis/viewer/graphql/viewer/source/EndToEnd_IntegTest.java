ded394e6ed8ff36b4093a758c2dae4c1537af729
Johan Doornenbal <johan@yodo.nl>
2022-04-08 12:53:18 +0200

    It seems end to end test is not using datafetchers properly

--- incubator/viewers/graphql/viewer/src/test/java/org/apache/isis/viewer/graphql/viewer/source/EndToEnd_IntegTest.java
+++ incubator/viewers/graphql/viewer/src/test/java/org/apache/isis/viewer/graphql/viewer/source/EndToEnd_IntegTest.java
@@ -2,12 +2,15 @@ package org.apache.isis.viewer.graphql.viewer.source;
 
 import org.apache.isis.core.config.environment.IsisSystemEnvironment;
 import org.apache.isis.core.metamodel.specloader.SpecificationLoader;
+import org.apache.isis.viewer.graphql.viewer.source.gqltestdomain.E1;
+import org.apache.isis.viewer.graphql.viewer.source.gqltestdomain.E2;
 import org.apache.isis.viewer.graphql.viewer.source.gqltestdomain.TestEntityRepository;
 import org.approvaltests.Approvals;
 import org.approvaltests.core.Options;
 import org.approvaltests.reporters.TextWebReporter;
 import org.approvaltests.reporters.UseReporter;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,10 +21,12 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Path;
+import java.util.List;
 
 import static java.net.http.HttpRequest.BodyPublishers.ofFile;
 import static java.net.http.HttpRequest.BodyPublishers.ofString;
 import static org.apache.isis.commons.internal.assertions._Assert.assertNotNull;
+import static org.apache.isis.commons.internal.assertions._Assert.assertTrue;
 
 
 @Transactional
@@ -44,6 +49,24 @@ public class EndToEnd_IntegTest extends TestDomainModuleIntegTestAbstract{
         assertNotNull(isisSystemEnvironment);
         assertNotNull(specificationLoader);
         assertNotNull(graphQlSourceForIsis);
+        E1 foo = testEntityRepository.createE1("foo", null);
+        testEntityRepository.createE2("bar", foo);
+        List<E1> allE1 = testEntityRepository.findAllE1();
+        assertTrue(allE1.size()==1);
+        List<E2> allE2 = testEntityRepository.findAllE2();
+        assertTrue(allE2.size()==1);
+    }
+
+    @Test
+    @Disabled("Creates schema.gql file for convenience")
+    void print_schema_works() throws Exception {
+
+        HttpClient client = HttpClient.newBuilder().build();
+        URI uri = URI.create("http://0.0.0.0:" + port + "/graphql/schema");
+        HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
+        File targetFile1 = new File("src/test/resources/testfiles/schema.gql");
+        HttpResponse<Path> response = client.send(request, HttpResponse.BodyHandlers.ofFile(targetFile1.toPath()));
+
     }
 
     @Test
@@ -58,6 +81,25 @@ public class EndToEnd_IntegTest extends TestDomainModuleIntegTestAbstract{
         HttpResponse<Path> response = client.send(request, HttpResponse.BodyHandlers.ofFile(targetFile2.toPath()));
 
         Approvals.verify(targetFile2, new Options());
+
+    }
+
+    @Test
+    void why_does_this_return_empty_list() throws Exception {
+
+        // given
+        List<E1> allE1 = testEntityRepository.findAllE1();
+        assertTrue(allE1.size()==1);
+
+        // when
+        File body2 = new File("src/test/resources/testfiles/body2.gql");
+        HttpClient client = HttpClient.newBuilder().build();
+        URI uri = URI.create("http://0.0.0.0:" + port + "/graphql");
+        HttpRequest request = HttpRequest.newBuilder().uri(uri).POST(ofFile(body2.toPath())).setHeader("Content-Type", "application/json").build();
+        File targetFile3 = new File("src/test/resources/testfiles/targetFile3.gql");
+        HttpResponse<Path> response = client.send(request, HttpResponse.BodyHandlers.ofFile(targetFile3.toPath()));
+
+//        Approvals.verify(targetFile3, new Options());
 
     }
 
